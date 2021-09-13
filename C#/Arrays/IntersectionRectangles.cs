9ab45e265191588a46dc5417d71eae46bb02011b
Mohammed Azmat Qureshi <v-moq@microsoft.com>
2021-09-13 13:12:44 +0530

    Added Optimized Arrays/InterSectingRectangles

--- C#/Arrays/IntersectionRectangles.cs
+++ C#/Arrays/IntersectionRectangles.cs
@@ -34,5 +34,28 @@ namespace Arrays
                 return true;
             return false;
         }
+
+        public class Point
+        {
+            public int x, y;
+            public Point(int _x, int _y)
+            {
+                x = _x;
+                y = _y;
+            }
+        }
+        public bool Overlap(List<List<int>> points)
+        {
+            Point l1 = new Point(points[0][0], points[0][1]);
+            Point r1 = new Point(points[1][0], points[1][1]);
+            Point l2 = new Point(points[2][0], points[2][1]);
+            Point r2 = new Point(points[3][0], points[3][1]);
+
+            if (l1.x >= r2.x || l2.x >= r1.x)
+                return false;
+            if (r1.y > l2.y || r2.y >= l1.y)
+                return false;
+            return true;
+        }
     }
 }

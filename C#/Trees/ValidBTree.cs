92edd68284cb54505e4facf9a332b153c57cebb3
Mohammed Azmat Qureshi <v-moq@microsoft.com>
2021-10-18 15:19:43 +0530

    Valid Binary Search Tree

---
+++ C#/Trees/ValidBTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    public class ValidBTree
+    {
+        //We do inorder travelsal and we keep track of prev node
+
+        public bool IsValidTree(Node root, Node left, Node right)
+        {
+            if (root == null)
+                return true;
+
+            if (left != null && root.value <= left.value)
+                return false;
+
+            if (right != null && root.value >= right.value)
+                return false;
+
+            return IsValidTree(root.left, left, root) && IsValidTree(root.right, root, right );
+        }
+    }
+}
